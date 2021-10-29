f0163c2fd8140757acb704561f40eed6416d16aa
gitcoder-23 <prabhatece21@gmail.com>
2021-10-29 16:17:31 +0530

    🐛

--- src/pages/__tests__/TodoList.test.tsx
+++ src/pages/__tests__/TodoList.test.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import TodoList from '../TodoList';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without error', () => {
-  const wrapper = shallow(
-    <Provider store={store}>
-      <TodoList />
-    </Provider>
-  );
-  const todoComponent = wrapper.find("[data-test='component-todolist']");
-  expect(todoComponent.length).toBe(0);
+// test('renders without error', () => {
+//   const wrapper = shallow(
+//     <Provider store={store}>
+//       <TodoList />
+//     </Provider>
+//   );
+//   const todoComponent = wrapper.find("[data-test='component-todolist']");
+//   expect(todoComponent.length).toBe(0);
+// });
+
+describe('<TodoList />', () => {
+  test('renders without error', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
+    );
+    const todoComponent = wrapper.find("[data-test='component-todolist']");
+    expect(todoComponent.length).toBe(0);
+  });
 });
